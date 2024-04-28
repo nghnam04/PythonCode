0a971a6da015a566e5b1bd9af36336a2ed5719b4
Nguyen Hoang Nam <hoangnam2004hp@gmail.com>
2024-04-28 18:33:27 +0700

    Test các chức năng lquan chuỗi

--- TestString2.py
+++ TestString2.py
@@ -1,8 +1,19 @@
 a = "Hello World!"
-#Test chỉ số mảng
+b = " 123"
+#Test chỉ số mảng + cắt chuỗi
 print(a[0])
 print(a[1])
 print(a[-1])
 print(a[-3])
 print(a[2:4])
 print(a[-5:])
+#Test nối chuỗi
+print(a*2)
+print(a+b)
+
+#Test tìm phần tử trong chuỗi
+s="HUST"
+c1 = 'S'
+c2 = 's'
+print(c1 in s)
+print(c2 not in s)
