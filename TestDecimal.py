6361eb438f4fda3c46271b1e1fe0bc4e747b6b76
Nguyen Hoang Nam <hoangnam2004hp@gmail.com>
2024-04-28 17:46:58 +0700

    Thêm các th khác

--- TestDecimal.py
+++ TestDecimal.py
@@ -1,3 +1,12 @@
 import decimal
 print(0.1)
 print(decimal.Decimal(0.1))
+
+print(0.2)
+print(decimal.Decimal(0.2))
+
+print(0.3)
+print(decimal.Decimal(0.3))
+
+print(0.1+0.2)
+print(decimal.Decimal(0.1+0.2))
